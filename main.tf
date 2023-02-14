f0d2cd668712d9afc232792a0deeb845bfd75c61
SirBizkit <popanda.pawel@gmail.com>
2023-02-14 20:26:26 +0000

    Added instance

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_instance" "controller" {
+  ami           = "ami-0d1ddd83282187d18" # Ubuntu 22.04 LTS in Frankfurt
+  instance_type = "t2.micro"
+}
