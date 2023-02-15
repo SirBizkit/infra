f0a1a1f53f08c695c99514eab293a7ff611d5c02
SirBizkit <popanda.pawel@gmail.com>
2023-02-15 20:34:11 +0000

    Renamed versioning resource

--- modules/s3/encrypted-private-s3-bucket/main.tf
+++ modules/s3/encrypted-private-s3-bucket/main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "s3" {
 }
 
 # Conditionally turn on file versioning
-resource "aws_s3_bucket_versioning" "enabled" {
+resource "aws_s3_bucket_versioning" "s3_versioning" {
   bucket = aws_s3_bucket.s3.id
   versioning_configuration {
     status = var.file_versioning ? "Enabled" : "Disabled"
