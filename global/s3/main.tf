82735402560745f716fc4e663d6999c9c061854b
SirBizkit <popanda.pawel@gmail.com>
2023-02-15 19:53:05 +0000

    Added tags to S3

--- global/s3/main.tf
+++ global/s3/main.tf
@@ -5,6 +5,10 @@ provider "aws" {
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "sirbizkit-infra-terraform-state"
 
+  tags = {
+    Name = "terraform_state"
+  }
+
   lifecycle {
     prevent_destroy = true # Prevent accidental deletion of this S3 bucket
   }
