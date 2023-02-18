ca018ff39ecbc4150a98e859a04b2f0aa50ccc14
SirBizkit <popanda.pawel@gmail.com>
2023-02-18 19:33:23 +0000

    Added basic k3s node installation commands

--- files/installK3sAgent.sh
+++ files/installK3sAgent.sh
@@ -1,1 +1,4 @@
-curl -sfL https://get.k3s.io | K3S_URL=https://myserver:6443 K3S_TOKEN=mynodetoken sh -
+until (curl -sfL https://get.k3s.io | K3S_URL=https://myserver:6443 K3S_TOKEN=mynodetoken sh -); do
+  echo 'Waiting for k3s installation'
+  sleep 3
+done
