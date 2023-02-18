325793e5bfdba867705ba9b0444a925d675cba7a
SirBizkit <popanda.pawel@gmail.com>
2023-02-18 19:32:37 +0000

    Added basic k3s server startup

--- files/installK3sServer.sh
+++ files/installK3sServer.sh
@@ -1,1 +1,10 @@
-curl -sfL https://get.k3s.io | sh -
+until (curl -sfL https://get.k3s.io | sh -); do
+  echo 'Waiting for k3s installation'
+  sleep 3
+done
+
+until kubectl get pods -A | grep 'Running';
+do
+  echo 'Waiting for k3s to initialize'
+  sleep 5
+done
