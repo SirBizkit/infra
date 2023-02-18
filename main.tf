f28251b0fe1a546948993dc54c4d75276f513dc8
SirBizkit <popanda.pawel@gmail.com>
2023-02-18 20:31:45 +0000

    Added bash copy and call for k3s install

--- main.tf
+++ main.tf
@@ -26,6 +26,17 @@ resource "aws_instance" "controller" {
   ami           = "ami-0abaf6cca7f5c0e6a" # Ubuntu 22.04 ARM LTS in Frankfurt
   instance_type = "t4g.small"
 
+  provisioner "file" {
+    source      = "files/installK3sServer.sh"
+    destination = "/tmp/installK3sServer.sh"
+  }
+
+  user_data= <<-EOF
+    #!/bin/bash
+    chmod +x /tmp/installK3sServer.sh
+    ./tmp/installK3sServer.sh
+  EOF
+
   tags = {
     Name = "controller"
   }
@@ -40,4 +51,8 @@ resource "aws_instance" "node" {
   tags = {
     Name = "node.${count.index}"
   }
+
+  depends_on = [
+    aws_instance.controller
+  ]
 }
