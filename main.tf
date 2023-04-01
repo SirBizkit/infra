8146e6a06f5ea846fac7e8ee24b585624c751426
SirBizkit <popanda.pawel@gmail.com>
2023-04-01 18:57:22 +0100

    Try adding ingress rules on the cluster

--- main.tf
+++ main.tf
@@ -66,13 +66,13 @@ resource "aws_security_group" "k3s_https_access" {
     {
       cidr_blocks      = [ "0.0.0.0/0", ]
       description      = "Dashboard port"
-      from_port        = 8443
+      from_port        = 443
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       protocol         = "tcp"
       security_groups  = []
       self             = false
-      to_port          = 8443
+      to_port          = 443
     }
   ]
 }
